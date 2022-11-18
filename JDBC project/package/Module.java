25574deb3f3e01a4b7b1d551b76b3b544d2995d0
Hantaro171902 <97072454+Hantaro171902@users.noreply.github.com>
2022-11-18 11:29:27 +0700

    Add files via upload

---
+++ JDBC project/package/Module.java
@@ -0,0 +1,23 @@
+package Demo;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import java.sql.*;
+
+public class Module {
+	Connection con;
+	PreparedStatement pst;
+
+	public void Connect() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/project","root","v5C59R@Z");
+			System.out.println("Connected");
+		}catch (Exception ex) { //exception handling (if any)
+			System.out.println("Cannot connect");
+			ex.printStackTrace();
+		}
+	}
+
+}
